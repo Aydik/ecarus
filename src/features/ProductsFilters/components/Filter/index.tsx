f4de317b0c031756cc7b60e625cfa4ffa35a7b3d
Aydik <Nekkisuper@gmail.com>
2025-05-16 20:44:42 +0300

    Select All Filter

--- src/features/ProductsFilters/components/Filter/index.tsx
+++ src/features/ProductsFilters/components/Filter/index.tsx
@@ -5,7 +5,7 @@ import { FilterFlags, FilterTypes } from 'features/ProductsFilters/types';
 import { Typography } from 'shared/ui/Typography';
 import { CheckBox } from 'shared/ui/CheckBox';
 import { PayloadAction } from '@reduxjs/toolkit';
-import { JSX } from 'react';
+import { JSX, useMemo } from 'react';
 
 interface Props<T extends FilterTypes> {
   title: string;
@@ -20,6 +20,11 @@ export const Filter = <T extends FilterTypes>({
 }: Props<T>): JSX.Element => {
   const dispatch: AppDispatch = useDispatch();
 
+  const hasSelectAll = title !== 'Пол';
+  const isAllSelected = useMemo(() => {
+    return (Object.values(filterFlags) as boolean[]).every((flag) => flag);
+  }, [filterFlags]);
+
   const handleChange = (key: T) => {
     const updated: FilterFlags<T> = {
       ...filterFlags,
@@ -28,13 +33,32 @@ export const Filter = <T extends FilterTypes>({
     dispatch(reducer(updated));
   };
 
+  const handleSelectAll = () => {
+    const updated: FilterFlags<T> = {} as FilterFlags<T>;
+    for (const key of Object.keys(filterFlags) as T[]) {
+      updated[key] = !isAllSelected;
+    }
+    dispatch(reducer(updated));
+  };
+
   return (
     <div className={styles.filter}>
       <Typography className={styles.filterCaption}>{title}</Typography>
       <ul
         className={styles.checkboxContainer}
-        style={{ marginTop: title === 'Пол' ? '16px' : '12px' }}
+        style={{ marginTop: hasSelectAll ? '12px' : '16px' }}
       >
+        {hasSelectAll && (
+          <li key={'all'} className={styles.item}>
+            <CheckBox
+              id={'selectAll ' + title}
+              checked={isAllSelected}
+              onChange={() => handleSelectAll()}
+            >
+              Выбрать все
+            </CheckBox>
+          </li>
+        )}
         {(Object.entries(filterFlags) as [T, boolean][]).map(([key, value]) => (
           <li key={key} className={styles.item}>
             <CheckBox id={key} checked={value} onChange={() => handleChange(key)}>
