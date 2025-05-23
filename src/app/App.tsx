7dddba962bff926abc1b2248b0c499332bc475b8
Aydik <Nekkisuper@gmail.com>
2025-05-23 04:08:38 +0300

    UserCard

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,15 +3,11 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { AppRouter } from './router/AppRouter';
 
 import './styles/index.module.scss';
-import { Provider } from 'react-redux';
-import { store } from './store';
 
 function App(): React.ReactElement {
   return (
     <Router>
-      <Provider store={store}>
-        <AppRouter />
-      </Provider>
+      <AppRouter />
     </Router>
   );
 }
