c662f4487d3934200bda5795b28774085b94718b
Aydik <Nekkisuper@gmail.com>
2025-06-09 23:40:42 +0300

    pg-pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import * as path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/ecarus/',
   resolve: {
     alias: {
       src: path.resolve(__dirname, './src'),
@@ -15,6 +16,18 @@ export default defineConfig({
       features: path.resolve(__dirname, './src/features'),
       entities: path.resolve(__dirname, './src/entities'),
       shared: path.resolve(__dirname, './src/shared'),
+    },
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name]-[hash][extname]',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js',
+      },
     },
   },
 });
